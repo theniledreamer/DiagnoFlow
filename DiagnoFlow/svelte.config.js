0f664958b5b47dab00f5541e2b8250767dc72e79
Aladdin <alyeldinshahin@gmail.com>
2025-01-17 15:58:28 -0500

    updating to adapter use

--- DiagnoFlow/svelte.config.js
+++ DiagnoFlow/svelte.config.js
@@ -1,8 +1,8 @@
-import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-vercel';
 
 const config = {
   kit: {
-    adapter: vercel({
+    adapter: adapter({
       // options here are optional
       runtime: 'nodejs20.x' // Ensure this matches Vercel's supported Node.js runtime
     })
